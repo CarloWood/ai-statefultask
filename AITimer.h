50d791e70c86a3158aa2b9257dc86fb00726a6ef
Carlo Wood <carlo@alinoe.com>
2019-12-02 21:02:52 +0100

    rewrite_header: add module name and desc.

--- AITimer.h
+++ AITimer.h
@@ -1,4 +1,6 @@
 /**
+ * ai-statefultask -- Asynchronous, Stateful Task Scheduler library.
+ *
  * @file
  * @brief Generate a timer event. Declaration of class AITimer.
  *
@@ -7,18 +9,20 @@
  * RSA-1024 0x624ACAD5 1997-01-26                    Sign & Encrypt
  * Fingerprint16 = 32 EC A7 B6 AC DB 65 A6  F6 F6 55 DD 1C DC FF 61
  *
- * This program is free software: you can redistribute it and/or modify
+ * This file is part of ai-statefultask.
+ *
+ * Ai-statefultask is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * Ai-statefultask is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with ai-statefultask.  If not, see <http://www.gnu.org/licenses/>.
  *
  * CHANGELOG
  *   and additional copyright holders.
