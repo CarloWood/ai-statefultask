e6d4721f6e3cf326188e020a64581bbb9b9350f0
Carlo Wood <carlo@alinoe.com>
2021-10-17 13:35:20 +0200

    Fix Release build.

--- BrokerKey.h
+++ BrokerKey.h
@@ -25,7 +25,9 @@ class BrokerKey
   virtual uint64_t hash() const = 0;
   virtual void initialize(boost::intrusive_ptr<AIStatefulTask> task) const = 0;
   virtual unique_ptr copy() const = 0;
+#ifdef CWDEBUG
   virtual void print_on(std::ostream& os) const = 0;
+#endif
 
  public:
   bool equal_to(BrokerKey const& other) const { return typeid(*this) == typeid(other) && equal_to_impl(other); }
